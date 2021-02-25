ad32c91dde813c8b46cf4e75c18bda5d6d0562b8
lb-beep <77352348+lb-beep@users.noreply.github.com>
2021-02-25 10:10:21 -0700

    Update Index.js

--- ModuleOne/wwwroot/js/Index.js
+++ ModuleOne/wwwroot/js/Index.js
@@ -1,1 +1,12 @@
+var h1Variable = $("MyPar");
+        $("#MyPar").css("color", "blue");
+        var span = $("<span></span>").text("Hello Man");
+        h1Variable.append(span);
 
+        var sectionElements = $(".SectionClass");
+        for (var index = 0; index < sectionElements.length; index++) {
+            $(sectionElements[index]).css = ("color", 'purple');
+        }
+       
+        $("<button></button>").attr("id", "MyButton")
+            .text("Click Me").appendTo("#MainContainerDiv");
